2229f74b23a5ad0e5ae0fdd2222698adfdaf5848
d.sokol <codic@mail.ru>
2017-03-22 19:40:15 +0300

    finalo4ka

--- RequestDemoLibrary.py
+++ RequestDemoLibrary.py
@@ -1,13 +1,8 @@
 import requests
 from requests.auth import HTTPBasicAuth
-import json
 
 
 class RequestDemoLibrary(object):
-    """
-    asd
-    """
-
     def __init__(self):
         self.url = "http://httpbin.org{}"
         self.get = "/get"
@@ -15,13 +10,13 @@ class RequestDemoLibrary(object):
         self.stream = "/stream/{}"
         self.response = None
 
-    def request_get(self):
+    def request_get(self, headers=None):
         """
-        Send request to httpbin.org/get
+        Send request to http://httpbin.org/get with custom headers value
+        :param headers: request headers
         :return: response to request
         """
-        headers = {'test': 'my-app/0.0.1'}
-        self.response = requests.get(self.url.format(self.get, headers=headers))
+        self.response = requests.get(self.url.format(self.get), headers=headers)
         return self
 
     def request_auth(self, user, passwd):
@@ -51,45 +46,29 @@ class RequestDemoLibrary(object):
         """
         return str(self.response.status_code)
 
-    def status_code_should_be(self, status_code):
-        actual_status_code = self.get_response_status_code()
-        if actual_status_code != int(status_code):
-            raise AssertionError('Actual Status code != Expected: %s != %s' % (actual_status_code, status_code))
-        return self
-
-    def number_of_lines_should_be(self, number_of_lines):
-        actual_number_of_lines = len(self.get_response_text().splitlines())
-        if actual_number_of_lines != int(number_of_lines):
-            raise AssertionError(
-                'Actual Number of lines != Expected: %s != %s' % (actual_number_of_lines, number_of_lines))
-        return self
-
-    def authorization_status_should_be(self, auth_status):
-        actual_auth_status = self.get_response_authorization_status()
-        if actual_auth_status != bool(auth_status):
-            raise AssertionError(
-                'Actual Authorization status != Expected: %s != %s' % (actual_auth_status, auth_status))
-        return self
-
     def get_response_text(self):
         """
-
-        :param response:
-        :return:
+        Get response text
+        :return: response text
         """
         return self.response.text
 
     def get_response_authorization_status(self):
+        """
+        Get response authorization status value from the text of response
+        :return: response authorization status
+        """
         if self.response.headers.get('content-type') == "application/json":
             return self.response.json()['authenticated']
         else:
-            raise AssertionError(
-                '%s != %s' % (self.response.headers.get('content-type'), str("application/json")))
+            return False
 
-
-r = RequestDemoLibrary()
-r.request_auth("asd", "qwezsd")
-print r.response.headers.get('content-type')
-print type(r.response.status_code)
-# print RequestDemoLibrary().request_auth("asd", "asd").response.text
-# print RequestDemoLibrary().get_response_line_count(RequestDemoLibrary().request_stream(99))
+    def get_response_header(self):
+        """
+        Get response header value from the text of response
+        :return: response header value
+        """
+        if self.response.headers.get('content-type') == "application/json":
+            return self.response.json()['headers']
+        else:
+            return False
--- test.robot
+++ test.robot
@@ -1,52 +1,60 @@
 *** Settings ***
-Documentation     A test suite with a single test for valid login.
-...
-...               This test has a workflow that is created using keywords in
-...               the imported resource file.
+Documentation     A test suite with a tests to assert http://httpbin.org/ servises.
+
 Library  RequestDemoLibrary.py
+Library  BuiltIn
+Library  String
+Suite Setup  Initialize header dictionary
 
 *** Variables ***
 ${STREAMCOUNT}    7
-${USERNAME}       200
-${PASSWORD}       200
 ${STATUSCODE}     200
 ${AUTH STATUS}    True
 
 *** Test Cases ***
-Valid Stream
-    test request stream
-
-Valid Auth
-    test request auth
-
-Valid Get
-    test request get
-
-Test auth
-    [Template]  auth and check status code
-    # name        # pwd    # expected response code
-    qwe           qwe      200
-    asd           asd      200
-
-*** Keywords ***
-Test request auth
-    Request Auth  ${USERNAME}   ${PASSWORD}
-    Status Code Should Be  ${STATUSCODE}
-    Authorization Status Should Be  ${AUTH STATUS}
-
 Test request stream
     Request Stream  ${STREAMCOUNT}
     Status Code Should Be  ${STATUSCODE}
     Number Of Lines Should Be  ${STREAMCOUNT}
 
 Test request get
-    Request Get
+    Log  ${HEADERS}
+    Request Get  ${HEADERS}
     Status Code Should Be  ${STATUSCODE}
-    ${result} =  Get Response Status Code
-    Status Code Should Be  ${result}
 
-Auth and check status code
-    [Arguments]  ${name}  ${pwd}  ${expected_response_code}
-    Request Auth  ${name}  ${pwd}
+Test request auth
+    [Template]  auth and check status code
+    # name        # pwd     # expected response code     #auth status
+    qwe           qwe       200                          True
+    asd           asd       200                          True
+    ${EMPTY}      pass      404                          False
+    login         ${EMPTY}  404                          False
+    ${EMPTY}      ${EMPTY}  404                          False
+
+*** Keywords ***
+Number Of Lines Should Be
+    [Arguments]  ${expected_stream_lines}
+    ${responce text}  Get Response Text
+    ${stream_lines}  Get Line Count  ${responce text}
+    Should Be Equal As Integers  ${stream_lines}  ${expected_stream_lines}
+
+Status Code Should Be
+    [Arguments]  ${expected_response_code}
     ${status}  Get Response Status Code
     Should Be Equal  ${status}  ${expected_response_code}
+
+Header Should Be
+    [Arguments]  ${expected_header}
+    ${header}  Get Response Header
+    Should Be Equal  ${header}  ${expected_header}
+
+Auth and check status code
+    [Arguments]  ${name}  ${pwd}  ${expected_response_code}  ${auth_status}
+    Request Auth  ${name}  ${pwd}
+    Status Code Should Be   ${expected_response_code}
+    ${auth_status}  Get Response Authorization Status
+    Should Be Equal  ${auth_status}  ${auth_status}
+
+Initialize header dictionary
+    ${HEADERS}=  Create Dictionary  test=qwe
+    Set Suite Variable  ${HEADERS}
